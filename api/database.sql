c3870e62a8b62191149676176c947252b37063c1
jordan byrd <jabyrd3@gmail.com>
2016-07-31 14:30:00 -0400

    first pass at data model

--- api/database.sql
+++ api/database.sql
@@ -1,1 +1,30 @@
 # put database startup scripts in here in lieu of any fancy migrations
+
+DROP TABLE "world_state";
+DROP TABLE "stops";
+DROP TABLE "users";
+DROP TABLE "monsters";
+CREATE TABLE "world_state"
+(
+	time TIMESTAMP,
+	seed VARCHAR(100)
+);
+CREATE TABLE "stops"
+(
+  coordinates POINT,
+  seed VARCHAR(100),
+  state VARCHAR(100)
+);
+CREATE TABLE "users"
+(
+  id UUID PRIMARY KEY ,
+  name VARCHAR(30)
+);
+CREATE TABLE "monsters"
+(
+  name VARCHAR(30),
+  encstats VARCHAR(100),
+  created TIMESTAMP,
+  caughtby UUID UNIQUE references users(id)
+);
+
